dbeb13910f51795734ef8810277226efdfb9fa0b
visualstudio <visualstudio@230-PC36>
2019-01-31 11:54:21 -0500

    changes

--- App1/js/main.js
+++ App1/js/main.js
@@ -1,1 +1,5 @@
 ﻿// Your code here!
+function myFunction() {
+    var date = new Date();
+    document.body.innerHTML = "<h1>The date is " + date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + " </h1>"
+}
